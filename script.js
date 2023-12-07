a37079290f074e2ab5e2865ae47441683bf626f9
JavrisMakeSense <106296031+JavrisMakeSense@users.noreply.github.com>
2023-12-07 12:43:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/**
+const jud = document.querySelector("h1.judul");
+const sel = confirm("on or off?");
+if (sel == 1) {
+  jud.innerHTML = `its ${sel}...`;
+  jud.style.color = "blue";
+  jud.style.backgroundColor = "red";
+} else {
+  jud.innerHTML = `its ${sel}...`;
+  jud.style.color = "red";
+  jud.style.backgroundColor = "blue";
+}
+*/
+
+
+
+
+
+
+
+
+
+  
+
+
+const jud = document.querySelector("#judul");
+
+
